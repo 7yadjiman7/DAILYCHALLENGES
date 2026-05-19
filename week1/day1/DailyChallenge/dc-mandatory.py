d271c5d4961709f371aa9df05635cd9f756a8192
7yadjiman7 <yadjimancoulibaly042@gmail.com>
2026-05-19 12:48:01 +0000

    day1 DailyChallenge

---
+++ week1/day1/DailyChallenge/dc-mandatory.py
@@ -0,0 +1,37 @@
+#Challenge 1
+# Ask the user for a number and a length.
+# Create a program that prints a list of multiples of the number until the list length reaches length.
+# Examples
+# number: 7 - length 5 ➞ [7, 14, 21, 28, 35]
+
+number = int(input("Please enter a number: "))
+length = int(input("Please enter a length: "))  
+multiples = []
+for i in range(1, length + 1):
+    multiples.append(number * i)
+print(multiples)
+
+
+# Challenge 2
+# Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+
+# user's word : "ppoeemm" ➞ "poem"
+
+# user's word : "wiiiinnnnd" ➞ "wind"
+
+# user's word : "ttiiitllleeee" ➞ "title"
+
+# user's word : "cccccaaarrrbbonnnnn" ➞ "carbon"
+
+
+# Notes
+# Final strings won’t include words with double letters (e.g. “passing”, “lottery”).
+
+user_string = input("Please enter a string: ")
+result_string = ""
+for i in range(len(user_string)):
+    if i == 0 or user_string[i] != user_string[i - 1]:
+        result_string += user_string[i]
+print(result_string)
+
+
